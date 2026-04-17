8da92a17a7fdb57c462256be6c5609314bc5cb49
christophboecker <christoph_boecker@t-online.de>
2026-04-17 11:34:33 +0200

    Fix typo in internationalization function call

--- pages/index.php
+++ pages/index.php
@@ -2,5 +2,5 @@
 
 /** @var rex_addon $this */
 
-echo rex_view::title(rex_18n::msg('markdowneditor_title'));
+echo rex_view::title(rex_i18n::msg('markdowneditor_title'));
 rex_be_controller::includeCurrentPageSubPath();
